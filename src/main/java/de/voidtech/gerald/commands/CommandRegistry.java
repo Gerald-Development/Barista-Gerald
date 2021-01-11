be7c1eb8d96c282f570b800f0c7b3e42f830c849
0xffset <rene.gottschalk02@gmail.com>
2021-01-11 18:13:21 +0100

    Fixed a bug in apod, added all the action commands(pat, hug, etc...) in a dynamic way

--- src/main/java/de/voidtech/gerald/commands/CommandRegistry.java
+++ src/main/java/de/voidtech/gerald/commands/CommandRegistry.java
@@ -4,36 +4,51 @@ import main.java.de.voidtech.gerald.commands.info.*;
 
 import main.java.de.voidtech.gerald.commands.fun.*;
 
+import java.lang.reflect.InvocationTargetException;
+
 public enum CommandRegistry {
 
-	PING("ping", PingCommand.class),
-	SAY("say", SayCommand.class),
-	SERVERINFO("serverinfo", ServerInfoCommand.class),
-	FACT("fact", FactCommand.class),
-	ASK("ask", AskCommand.class),
-	WHOIS("whois", WhoisCommand.class),
-	COMPILE("compile", CompileCommand.class),
-	INSPIRO("inspiro", InspiroCommand.class),
-	APOD("apod", ApodCommand.class),
+	PING("ping", PingCommand.class, null),
+	SAY("say", SayCommand.class, null),
+	SERVERINFO("serverinfo", ServerInfoCommand.class, null),
+	FACT("fact", FactCommand.class, null),
+	ASK("ask", AskCommand.class, null),
+	WHOIS("whois", WhoisCommand.class, null),
+	COMPILE("compile", CompileCommand.class, null),
+	INSPIRO("inspiro", InspiroCommand.class, null),
+	APOD("apod", ApodCommand.class, null),
+	CUDDLE("cuddle", ActionsCommand.class, "cuddle"),
+	HUG("hug", ActionsCommand.class, "hug"),
+	KISS("kiss", ActionsCommand.class, "kiss"),
+	PAT("pat", ActionsCommand.class, "pat"),
+	POKE("poke", ActionsCommand.class, "poke"),
+	SLAP("slap", ActionsCommand.class, "slap"),
 
 	@SuppressWarnings("deprecation")
-	JUNIT_TEST("junittest", TestCommand.class),
+	JUNIT_TEST("junittest", TestCommand.class, null),
 
 	;
 
 	private String name;
 	private Class<? extends AbstractCommand> commandClass;
+	private String arg;
 
-	private CommandRegistry(String name, Class<? extends AbstractCommand> command) {
+	private CommandRegistry(String name, Class<? extends AbstractCommand> command, String arg) {
 		this.name = name;
 		this.commandClass = command;
+		this.arg = arg;
 	}
+
+
 
 	public String getName() {
 		return this.name;
 	}
 
-	public Class<? extends AbstractCommand> getCommandClass() {
-		return this.commandClass;
+	public AbstractCommand getCommand() throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+		if (this.arg == null)
+			return this.commandClass.newInstance();
+		else
+			return this.commandClass.getDeclaredConstructor(String.class).newInstance(arg);
 	}
 }
