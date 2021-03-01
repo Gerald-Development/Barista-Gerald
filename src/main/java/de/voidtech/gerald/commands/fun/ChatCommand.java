5c6b89b18d4fc37455b68c4e4f9e93d3145d67b2
ElementalMP4 <seebeesbt@gmail.com>
2021-03-01 08:37:40 +0000

    CLeaned up after PF replacement

--- src/main/java/de/voidtech/gerald/commands/fun/ChatCommand.java
+++ src/main/java/de/voidtech/gerald/commands/fun/ChatCommand.java
@@ -83,7 +83,7 @@ public class ChatCommand extends AbstractCommand{
 
 	@Override
 	public String getDescription() {
-		return "This command allows you to enable automatic replies from GeraldAI! (Powered by PersonalityForge)";
+		return "This command allows you to enable automatic replies from GeraldAI! (Powered by our own AI)";
 	}
 	@Override
 	public String getUsage() {
