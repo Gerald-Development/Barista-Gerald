8e61db2c995d8dcf3722863dc1c936be458c25a5
Sebastian <55952008+Elementalmp4@users.noreply.github.com>
2021-03-02 17:46:45 +0000

    confused confusing confusion noises
    The service changes got merged somehow and things got messed up real good

--- src/main/java/de/voidtech/gerald/service/ChatbotService.java
+++ src/main/java/de/voidtech/gerald/service/ChatbotService.java
@@ -11,23 +11,14 @@ import org.springframework.stereotype.Service;
 public class ChatbotService {	
 	
 	private Map<String, Chat> chatInstances = new HashMap<String, Chat>();
-<<<<<<< HEAD
-	
+
 	private Bot GERALD_AI = new Bot("gerald", "AIML");
-=======
-	private final String URI = getClass().getClassLoader().getResource("ai").getPath();
-	private Bot geraldAI;
-	
-	public ChatbotService() {
-		this.geraldAI = new Bot("Gerald", URI);
-	}
->>>>>>> df632a14c1e263cdf41bda035487c530da5b456b
-	
+
 	private Chat getChatInstance(String userID) {
 		if (chatInstances.containsKey(userID)) {
 			return chatInstances.get(userID);	
 		} else {
-		    Chat chatSession = new Chat(geraldAI);
+		    Chat chatSession = new Chat(GERALD_AI);
 			chatInstances.put(userID, chatSession);
 			return chatSession;
 		}
