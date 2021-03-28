0ae2d301979292cebb4937bb36e84d568e68040f
ElementalMP4 <seebeesbt@gmail.com>
2021-03-28 14:42:38 +0100

    Added ThreadManager

---
+++ src/main/java/de/voidtech/gerald/service/ThreadManager.java
@@ -0,0 +1,32 @@
+package main.java.de.voidtech.gerald.service;
+
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ThreadManager {
+	
+	private HashMap<String, ExecutorService> threadMap = new HashMap<String, ExecutorService>();
+
+	private ExecutorService findOrSpawnThread(String threadID) {
+		
+		if (!threadMap.containsKey(threadID)) {
+			BasicThreadFactory factory = new BasicThreadFactory.Builder()
+				     .namingPattern(threadID + "-%d")
+				     .daemon(true)
+				     .priority(Thread.NORM_PRIORITY)
+				     .build();
+			threadMap.put(threadID, Executors.newSingleThreadExecutor(factory));	
+		}
+		
+		return threadMap.get(threadID);
+	}
+	
+	public ExecutorService getThreadByName(String name) {
+		return findOrSpawnThread(name);
+	}
+}
