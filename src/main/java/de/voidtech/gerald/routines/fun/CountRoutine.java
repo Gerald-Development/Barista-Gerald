c0d3526af238287d883a6bed0dfd83d6a4bb9b1c
ElementalMP4 <seebeesbt@gmail.com>
2021-04-08 09:29:26 +0100

    Added NICEfunctionality to count routine

--- src/main/java/de/voidtech/gerald/routines/fun/CountRoutine.java
+++ src/main/java/de/voidtech/gerald/routines/fun/CountRoutine.java
@@ -14,6 +14,10 @@ import net.dv8tion.jda.api.entities.Message;
 public class CountRoutine extends AbstractRoutine {
 	private final static String CORRECT = "U+2705";
 	private final static String INCORRECT = "U+274E";
+	private final static String LETTER_N = "U+1F1F3";
+	private final static String LETTER_I = "U+1F1EE";
+	private final static String LETTER_C = "U+1F1E8";
+	private final static String LETTER_E = "U+1F1EA";
 	
 	@Autowired
 	private SessionFactory sessionFactory;
@@ -35,6 +39,16 @@ public class CountRoutine extends AbstractRoutine {
                     .setParameter("channelID", channelID)
                     .uniqueResult();
 			return !dbChannel.getLastUser().equals(userID);
+		}	
+	}
+	
+	private boolean hasChannelReached69(String channelID) {
+		try(Session session = sessionFactory.openSession())
+		{
+			CountingChannel dbChannel = (CountingChannel) session.createQuery("FROM CountingChannel WHERE ChannelID = :channelID")
+                    .setParameter("channelID", channelID)
+                    .uniqueResult();
+			return dbChannel.hasReached69();
 		}	
 	}
 	
@@ -79,12 +93,41 @@ public class CountRoutine extends AbstractRoutine {
                     .uniqueResult();
 			dbChannel.setChannelCount(0);
 			dbChannel.setLastUser("");
+			dbChannel.setReached69(false);
 			
 			session.saveOrUpdate(dbChannel);
 			session.getTransaction().commit();			
 		}	
 	}
 	
+	private boolean shouldSendNice(int countGiven, String channelID) {
+		return ((countGiven == 69 || countGiven == -69) && !hasChannelReached69(channelID));
+	}
+	
+	private void update69ReachedStatus(String channelID) {
+		try(Session session = sessionFactory.openSession())
+		{
+			session.beginTransaction();
+			CountingChannel dbChannel = (CountingChannel) session.createQuery("FROM CountingChannel WHERE ChannelID = :channelID")
+                    .setParameter("channelID", channelID)
+                    .uniqueResult();
+			
+			dbChannel.setReached69(true);
+			
+			session.saveOrUpdate(dbChannel);
+			session.getTransaction().commit();			
+		}	
+	}
+	
+	private void sendNice(Message message) {
+		message.addReaction(LETTER_N).queue();
+		message.addReaction(LETTER_I).queue();
+		message.addReaction(LETTER_C).queue();
+		message.addReaction(LETTER_E).queue();
+		
+		update69ReachedStatus(message.getChannel().getId());
+	}
+
 	@Override
 	public void executeInternal(Message message) {		
 		if (isCountingChannel(message.getChannel().getId())) {
@@ -96,14 +139,21 @@ public class CountRoutine extends AbstractRoutine {
 					if (countGiven == currentCount + 1) {
 						setCount(currentCount, message.getChannel().getId(), message.getMember().getId(), "increment");
 						message.addReaction(CORRECT).queue();
+						if (shouldSendNice(countGiven, message.getChannel().getId())) {
+							sendNice(message);
+						}
 					} else if (countGiven == currentCount - 1) {
 						setCount(currentCount, message.getChannel().getId(), message.getMember().getId(), "decrement");
 						message.addReaction(CORRECT).queue();
+						if (shouldSendNice(countGiven, message.getChannel().getId())) {
+							sendNice(message);
+						}
 					}else {
 						resetCount(message.getChannel().getId());
 						message.addReaction(INCORRECT).queue();
 						message.getChannel().sendMessage("**You failed! The counter has been reset!**").queue();
 					}		
+					
 				} else {
 					message.getChannel().sendMessage("**You cannot count twice in a row! The counter has not been reset.**").queue();
 					message.addReaction(INCORRECT).queue();
