6c48f4b05a6ece890aa63205b023b042d8128c1e
ElementalMP4 <seebeesbt@gmail.com>
2021-04-21 13:50:04 +0100

    Fixed ping responder

--- src/main/java/de/voidtech/gerald/routines/utils/PingResponseRoutine.java
+++ src/main/java/de/voidtech/gerald/routines/utils/PingResponseRoutine.java
@@ -11,6 +11,7 @@ import main.java.de.voidtech.gerald.routines.AbstractRoutine;
 import main.java.de.voidtech.gerald.service.GeraldConfig;
 import main.java.de.voidtech.gerald.service.ServerService;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 
@@ -25,18 +26,27 @@ public class PingResponseRoutine extends AbstractRoutine {
 	
 	@Override
 	public void executeInternal(Message message) {
-		if (message.getMentionedUsers().contains(message.getJDA().getSelfUser())) {
-			Server guild = serverService.getServer(message.getGuild().getId());
-			String prefix = guild.getPrefix() == null ? config.getDefaultPrefix() : guild.getPrefix();
-			new GlobalConstants();
-			String linktree = GlobalConstants.LINKTREE_URL;
+		
+		if (message.getChannelType().equals(ChannelType.TEXT)) {
+			String strippedContent = message.getContentRaw()
+					.replaceAll("<", "")
+					.replaceAll("!", "")
+					.replaceAll("@", "")
+					.replaceAll(">", "");
 			
-			MessageEmbed pingResponseEmbed = new EmbedBuilder()
-					.setColor(Color.ORANGE)
-					.setTitle("You called? :telephone:", linktree)
-					.setDescription("**This Guild's prefix is:** " + prefix + "\nTry " + prefix + "help to see some commands!")
-					.build();
-			message.getChannel().sendMessage(pingResponseEmbed).queue();
+			if (strippedContent.equals(message.getJDA().getSelfUser().getId())) {
+				Server guild = serverService.getServer(message.getGuild().getId());
+				String prefix = guild.getPrefix() == null ? config.getDefaultPrefix() : guild.getPrefix();
+				new GlobalConstants();
+				String linktree = GlobalConstants.LINKTREE_URL;
+				
+				MessageEmbed pingResponseEmbed = new EmbedBuilder()
+						.setColor(Color.ORANGE)
+						.setTitle("You called? :telephone:", linktree)
+						.setDescription("**This Guild's prefix is:** " + prefix + "\nTry " + prefix + "help to see some commands!")
+						.build();
+				message.getChannel().sendMessage(pingResponseEmbed).queue();
+			}	
 		}
 	}
 
