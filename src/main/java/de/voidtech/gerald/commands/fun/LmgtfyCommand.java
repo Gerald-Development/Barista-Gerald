bf7143cd98b62107c3a0bc296073ff65cc6cd10c
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-05-31 10:54:31 +0100

    Implement the command.

--- src/main/java/de/voidtech/gerald/commands/fun/LmgtfyCommand.java
+++ src/main/java/de/voidtech/gerald/commands/fun/LmgtfyCommand.java
@@ -1,4 +1,57 @@
 package main.java.de.voidtech.gerald.commands.fun;
 
-public class LmgtfyCommand {
+import main.java.de.voidtech.gerald.annotations.Command;
+import main.java.de.voidtech.gerald.commands.AbstractCommand;
+import main.java.de.voidtech.gerald.commands.CommandCategory;
+import net.dv8tion.jda.api.entities.Message;
+
+import java.util.List;
+
+@Command
+public class LmgtfyCommand extends AbstractCommand {
+
+    @SuppressWarnings("FieldCanBeLocal")
+    private final String googleQuery = "https://www.google.com/search?q=";
+
+    @Override
+    public void executeInternal(Message message, List<String> args) {
+        String replyURL = googleQuery + String.join("+", args);
+        message.getChannel().sendMessage(replyURL).queue();
+    }
+
+    @Override
+    public String getDescription() {
+        return "LMGTFY (Let Me Google That For You), when someone asks a very simple question that google can answer. Use this.";
+    }
+
+    @Override
+    public String getUsage() {
+        return "lmgtfy <query>";
+    }
+
+    @Override
+    public String getName() {
+        return "lmgtfy";
+    }
+
+    @Override
+    public CommandCategory getCommandCategory() {
+        return CommandCategory.FUN;
+    }
+
+    @Override
+    public boolean isDMCapable() {
+        return false;
+    }
+
+    @Override
+    public boolean requiresArguments() {
+        return true;
+    }
+
+    @Override
+    public String[] getCommandAliases() {
+        String[] aliases = {"lmgtfy", "google"};
+        return aliases;
+    }
 }
