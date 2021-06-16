60d9a636b180b6919060853778781875b603fe69
ElementalMP4 <seebeesbt@gmail.com>
2021-06-16 18:49:54 +0100

    feat(command): Upgraded HexCommand (#101)

--- src/main/java/de/voidtech/gerald/commands/utils/HexCommand.java
+++ src/main/java/de/voidtech/gerald/commands/utils/HexCommand.java
@@ -1,7 +1,13 @@
 package main.java.de.voidtech.gerald.commands.utils;
 
 import java.awt.Color;
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import main.java.de.voidtech.gerald.annotations.Command;
 import main.java.de.voidtech.gerald.commands.AbstractCommand;
@@ -15,6 +21,8 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 public class HexCommand extends AbstractCommand {
 
 	private static final String IMAGE_SOURCE_URL = "https://via.placeholder.com/250/";
+	private static final String COLOUR_HEXA_BASE_URL = "https://www.colorhexa.com/";
+	private static final Logger LOGGER = Logger.getLogger(HexCommand.class.getName());
 	
 	private Color getEmbedColour(String hex) {
 		return new Color(
@@ -25,14 +33,29 @@ public class HexCommand extends AbstractCommand {
 	
 	private void sendHexImage(Message message, String hexCode) {
 		String finalImageURL = IMAGE_SOURCE_URL + hexCode + "/" + hexCode + ".png";
+		String colourHexaURL = COLOUR_HEXA_BASE_URL + hexCode;
+		String colourName = getColourName(colourHexaURL);
+		
 		MessageEmbed hexColourEmbed = new EmbedBuilder()
 				.setColor(getEmbedColour(hexCode))
 				.setImage(finalImageURL)
-				.setTitle(hexCode.toUpperCase(), finalImageURL)
+				.setTitle("#" + hexCode.toUpperCase(), colourHexaURL)
+				.setFooter(colourName)
 				.build();
 		message.getChannel().sendMessage(hexColourEmbed).queue();
 	}
 	
+	private String getColourName(String colourHexaURL) {
+		try {
+			Document colourHexaPage = Jsoup.connect(colourHexaURL).get();
+			return colourHexaPage.select("#information > div.color-description > p > strong").first().text();
+		} catch (IOException e) {
+			LOGGER.log(Level.SEVERE, "Error during CommandExecution: " + e.getMessage());
+			e.printStackTrace();
+		}
+		return "";
+	}
+
 	@Override
 	public void executeInternal(Message message, List<String> args) {
 		String hexCode = args.get(0).replaceAll("#", "");
