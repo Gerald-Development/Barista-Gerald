dc4470f316fe4cdcb264ceacd9b28b2ffa692033
ElementalMP4 <seebeesbt@gmail.com>
2021-06-19 14:26:42 +0100

    fix(service): TwitchNotificationService should now be able to send notifications. Awaiting command (#106)

--- src/main/java/de/voidtech/gerald/service/TwitchNotificationService.java
+++ src/main/java/de/voidtech/gerald/service/TwitchNotificationService.java
@@ -11,6 +11,11 @@ import org.springframework.stereotype.Service;
 
 import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.TwitchClientBuilder;
+import com.github.twitch4j.events.ChannelGoLiveEvent;
+
+import main.java.de.voidtech.gerald.entities.TwitchNotificationChannel;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.TextChannel;
 
 @Service
 public class TwitchNotificationService {
@@ -23,25 +28,52 @@ public class TwitchNotificationService {
 	@Autowired
 	private GeraldConfig geraldConfig;
 	
+	@Autowired
+	private JDA jda;
+	
 	private TwitchClient twitchClient;
 	
 	public void subscribeToAllStreamers() {
-		LOGGER.log(Level.INFO, "Adding Twitch API subscriptions");
+		String twitchClientId = geraldConfig.getTwitchClientId();
+		String twitchClientSecret = geraldConfig.getTwitchSecret();
 		
-		twitchClient = TwitchClientBuilder.builder()
-				.withEnableHelix(true)
-				.withClientId(geraldConfig.getTwitchClientId())
-				.withClientSecret(geraldConfig.getTwitchSecret())
-				.build();
-		
-		List<String> streamerNames = getAllStreamerNames();
-		if (streamerNames != null) {
-			for (String name : streamerNames) {
-				twitchClient.getClientHelper().enableStreamEventListener(name);
+		if (twitchClientId != null && twitchClientSecret != null) {
+			LOGGER.log(Level.INFO, "Adding Twitch API subscriptions");
+			twitchClient = TwitchClientBuilder.builder()
+					.withEnableHelix(true)
+					.withClientId(twitchClientId)
+					.withClientSecret(twitchClientSecret)
+					.build();
+			
+			List<String> streamerNames = getAllStreamerNames();
+			if (streamerNames != null) {
+				for (String name : streamerNames) {
+					twitchClient.getClientHelper().enableStreamEventListener(name);
+				}
 			}
+			
+			LOGGER.log(Level.INFO, "Added Twitch API subscriptions!");
+			listenForTwitchEvents();
 		}
-		
-		LOGGER.log(Level.INFO, "Added Twitch API subscriptions!");
+	}
+
+	private void listenForTwitchEvents() {
+		twitchClient.getEventManager().onEvent(ChannelGoLiveEvent.class, event -> {
+			String streamerName = event.getChannel().getName();
+			List<TwitchNotificationChannel> channels = getNotificationChannelsByStreamerName(streamerName);
+			for (TwitchNotificationChannel channel : channels) {
+				sendNotificationMessage(channel, jda);
+			}
+		});
+	}
+
+	private void sendNotificationMessage(TwitchNotificationChannel channel, JDA jda) {
+		TextChannel notificationChannel = jda.getTextChannelById(channel.getChannelId());
+		notificationChannel.sendMessage(formMessage(channel)).queue();
+	}
+
+	private CharSequence formMessage(TwitchNotificationChannel channel) {
+		return channel.getNotificationMessage() + "\nhttps://twitch.tv/" + channel.getStreamerName();
 	}
 
 	@SuppressWarnings("unchecked")
@@ -51,6 +83,17 @@ public class TwitchNotificationService {
 			List<String> names = (List<String>) session.createQuery("SELECT DISTINCT streamerName FROM TwitchNotificationChannel")
                     .list();
 			return names;
+		}	
+	}
+	
+	@SuppressWarnings("unchecked")
+	private List<TwitchNotificationChannel> getNotificationChannelsByStreamerName(String streamerName) {
+		try(Session session = sessionFactory.openSession())
+		{
+			List<TwitchNotificationChannel> channels = (List<TwitchNotificationChannel>) session.createQuery("FROM TwitchNotificationChannel WHERE streamerName = :streamerName")
+					.setParameter("streamerName", streamerName)
+                    .list();
+			return channels;
 		}	
 	}
 	
