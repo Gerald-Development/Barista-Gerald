f72617ba58e28b6d9c48d9ca36f33ef765b34ee8
ElementalMP4 <seebeesbt@gmail.com>
2021-07-02 11:47:08 +0100

    feat(command): Implemented screenshotPage into GoogleCommand (#129)

--- src/main/java/de/voidtech/gerald/commands/utils/GoogleCommand.java
+++ src/main/java/de/voidtech/gerald/commands/utils/GoogleCommand.java
@@ -8,7 +8,6 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
-import com.microsoft.playwright.Page;
 
 import main.java.de.voidtech.gerald.annotations.Command;
 import main.java.de.voidtech.gerald.commands.AbstractCommand;
@@ -104,9 +103,9 @@ public class GoogleCommand extends AbstractCommand {
 			}, 60, TimeUnit.SECONDS, () -> {});
 	}
 	
-	private void sendFinalMessage(Message message, String url, byte[] screenshotBytesBuffer) {
+	private void sendFinalMessage(Message message, String url, byte[] screenshot) {
 		message.getChannel().sendMessageEmbeds(constructResultEmbed(url, getNsfwMode(message.getChannel())))
-		.addFile(screenshotBytesBuffer, "screenshot.png")
+		.addFile(screenshot, "screenshot.png")
 		.queue(sentMessage -> {
 			if (!message.getChannelType().equals(ChannelType.PRIVATE)) {
 				sendDeleteButtonWithListener(sentMessage, message);
@@ -140,14 +139,9 @@ public class GoogleCommand extends AbstractCommand {
 			message.getChannel().sendMessage("**You did not provide something to search for!**").queue();
 		else {
 			message.getChannel().sendTyping().queue();
-			Page searchPage = playwrightService.getBrowser().newPage();
-			searchPage.navigate(url);
-			searchPage.setViewportSize(1000, 1000);
+			byte[] screenshot = playwrightService.screenshotPage(url, 1000, 1000);	
 			
-			byte[] screenshotBytesBuffer = searchPage.screenshot();
-			searchPage.close();	
-			
-			sendFinalMessage(message, url, screenshotBytesBuffer);
+			sendFinalMessage(message, url, screenshot);
 		}
 	}
 
