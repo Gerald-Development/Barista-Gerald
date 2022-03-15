c1374b371eea988108e5b75843828462aadb642b
Seb <seebeesbt@gmail.com>
2022-03-15 17:39:08 +0000

    meta: Incremented version (i think its about time)

--- src/main/java/de/voidtech/gerald/GlobalConstants.java
+++ src/main/java/de/voidtech/gerald/GlobalConstants.java
@@ -4,5 +4,5 @@ public class GlobalConstants {
 	public static final String STREAM_URL = "https://twitch.tv/elementalmp4"; 
 	public static final String LINKTREE_URL = "https://linktr.ee/GeraldBot";
 	public static final String INVITE_URL = "https://discord.com/api/oauth2/authorize?client_id=555816892141404163&permissions=805694544&scope=bot";
-	public static final String VERSION = "1.2.7 - Mucho Mocha";
+	public static final String VERSION = "1.2.8 - Mucho Mocha";
 }
