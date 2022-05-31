6ec502bec8449acdcd3cfc665af4ab7929815925
Seb <seebeesbt@gmail.com>
2022-05-31 01:45:39 +0100

    refac(service): Added message reply embed to webhooks

--- src/main/java/de/voidtech/gerald/service/WebhookManager.java
+++ src/main/java/de/voidtech/gerald/service/WebhookManager.java
@@ -1,21 +1,28 @@
 package main.java.de.voidtech.gerald.service;
 
-import main.java.de.voidtech.gerald.commands.CommandContext;
-import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.GuildChannel;
-import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.entities.Webhook;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import javax.net.ssl.HttpsURLConnection;
+import java.awt.Color;
 import java.io.OutputStream;
 import java.net.URL;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import main.java.de.voidtech.gerald.commands.CommandContext;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.GuildChannel;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.Webhook;
 
 @Service
 public class WebhookManager {
@@ -36,7 +43,7 @@ public class WebhookManager {
 		return targetChannel.createWebhook(webhookName).complete();
 	}
 	
-	private void executeWebhookPost(String content, String avatarUrl, String username, Webhook webhook) {
+	private void executeWebhookPost(String content, Message referencedMessage, String avatarUrl, String username, Webhook webhook) {
 		String messageToBeSent = content.replaceAll("@everyone", "``@``everyone").replaceAll("@here", "``@``here");
 		
 		JSONObject webhookPayload = new JSONObject();
@@ -44,8 +51,19 @@ public class WebhookManager {
         webhookPayload.put("username", username);
         webhookPayload.put("avatar_url", avatarUrl);
         webhookPayload.put("tts", false);
+        
+		
+		if (referencedMessage != null) {
+			 MessageEmbed replyTextEmbed = new EmbedBuilder()
+					.setTitle("Replying to this message", referencedMessage.getJumpUrl())
+					.setDescription(referencedMessage.getContentRaw())
+					.setColor(new Color(47,49,54))
+					.setFooter("Original message from " + referencedMessage.getAuthor().getName(), referencedMessage.getAuthor().getAvatarUrl())
+					.build();
+			 webhookPayload.put("embeds", new JSONArray().put(new JSONObject(replyTextEmbed.toData().toString())));
+		}
+        
         try {              			
-        	
             URL url = new URL(webhook.getUrl());
             HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
             
@@ -53,7 +71,7 @@ public class WebhookManager {
             connection.addRequestProperty("User-Agent", "Barista-Gerald");
             connection.setDoOutput(true);
             connection.setRequestMethod("POST");
-
+            
             OutputStream stream = connection.getOutputStream();
             stream.write(webhookPayload.toString().getBytes());
             stream.flush();
@@ -68,8 +86,8 @@ public class WebhookManager {
         }
 	}
 	
-	public void postMessage(String content, String avatarUrl, String username, Webhook webhook) {
-        Runnable webhookThreadRunnable = () -> executeWebhookPost(content, avatarUrl, username, webhook);
+	public void postMessage(String content, Message referencedMessage, String avatarUrl, String username, Webhook webhook) {
+        Runnable webhookThreadRunnable = () -> executeWebhookPost(content, referencedMessage, avatarUrl, username, webhook);
 		threadManager.getThreadByName("T-Webhook").execute(webhookThreadRunnable);   
 	}
 	
@@ -77,10 +95,7 @@ public class WebhookManager {
 		EnumSet<Permission> perms = context.getGuild().getSelfMember().getPermissions((GuildChannel) context.getChannel());
 		
         if (perms.contains(Permission.MANAGE_WEBHOOKS)) {
-        	postMessage(
-        			content,
-        			avatarUrl,
-        			username,
+        	postMessage(content, null,	avatarUrl, username,
         			getOrCreateWebhook((TextChannel) context.getChannel(),
         					webhookName,
         					context.getJDA().getSelfUser().getId())
