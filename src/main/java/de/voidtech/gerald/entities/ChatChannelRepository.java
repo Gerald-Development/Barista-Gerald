23500a6aac1d2670477fca00ab1d8ed89143c467
ElementalMP4 <sebastian.beattie@bbc.co.uk>
2023-04-07 20:46:51 +0100

    Migrated ChatChannel to JpaRepository

---
+++ src/main/java/de/voidtech/gerald/entities/ChatChannelRepository.java
@@ -0,0 +1,20 @@
+package main.java.de.voidtech.gerald.entities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+public interface ChatChannelRepository extends JpaRepository<ChatChannel, Long> {
+
+    @Query("FROM ChatChannel WHERE ChannelID = :channelID")
+    ChatChannel getChatChannelByChannelId(String channelID);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM ChatChannel WHERE ChannelID = :channelID")
+    void deleteChatChannelByChannelId(String channelID);
+}
